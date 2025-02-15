4ee4ae29faa2428267e3e7af01540fee567540cf
CrudyLame <dopeyslimeg@gmail.com>
2025-02-15 06:59:50 +0300

    Prefinal commit

--- TgNotifyService/src/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.config import settings
-from src.utils.message_builder import MessageBuilder
-
-MESSAGE_BUILDER = MessageBuilder(settings.MESSAGE_TEMPLATES_CONFIG)
-
-__all__ = ["MESSAGE_BUILDER"]
